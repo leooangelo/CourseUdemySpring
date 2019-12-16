fb879043766d1491b0a8421ee67a0d742adaddbf
leooangelo <leozinhosp321@hotmail.com>
2019-12-16 16:09:14 -0300

    PedidoItem pt1

---
+++ src/main/java/com/example/course/entity/PedidoItem.java
@@ -0,0 +1,91 @@
+package com.example.course.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.example.course.entity.pk.PedidoPK;
+
+@Entity
+@Table(name = "tb_order_item")
+public class PedidoItem implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@EmbeddedId
+	private PedidoPK id;
+	
+	private Integer quantity;
+	private Double price;
+	
+	
+	public PedidoItem(Pedido order,Produto product,Integer quantity, Double price) {
+		super();
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	public Pedido getPedido() {
+		return id.getOrder();
+	}
+	public void setPedido(Pedido order) {
+		id.setOrder(order);
+	}
+	
+	public Produto getProduto() {
+		return id.getProduct();
+	}
+	
+	public void setProduto(Produto product) {
+		id.setProduct(product);
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((quantity == null) ? 0 : quantity.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PedidoItem other = (PedidoItem) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (quantity == null) {
+			if (other.quantity != null)
+				return false;
+		} else if (!quantity.equals(other.quantity))
+			return false;
+		return true;
+	}
+	
+	
+}
