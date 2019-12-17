360e27c26c08adbfb094183e1fc9303b7290ace0
leooangelo <leozinhosp321@hotmail.com>
2019-12-17 14:21:42 -0300

    Tratamento de Exception Delete

--- src/main/java/com/example/course/service/UserService.java
+++ src/main/java/com/example/course/service/UserService.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.course.entity.User;
+import com.example.course.entity.resources.exceptions.StandardError;
 import com.example.course.repository.UserRepository;
+import com.example.course.service.exceptions.DataBaseException;
 import com.example.course.service.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +34,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		userRepository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 	public User update(Long id, User obj) {
 		User entity = userRepository.getOne(id);
